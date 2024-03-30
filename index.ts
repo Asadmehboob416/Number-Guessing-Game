d31a1bcffe9818d3d068edb6e5b66b85dad43c07
Asad Mehboob <163368482+Asadmehboob416@users.noreply.github.com>
2024-03-31 02:48:27 +0500

    simple number guessing game
    its a simple game

---
+++ index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 6 +1);
+
+console.log("Wellcome to Number Guess Game");
+
+const answer = await inquirer.prompt([
+  {
+    name: "userGuessnumber",
+    type: "number",
+    message: "Please Guess a Number between [1-6] :- ",
+  },
+]);
+
+//conditionall statement
+if (answer.userGuessnumber === randomNumber) {
+  console.log("Congratulatios! You guessed Number.");
+} else {
+  console.log("You Guessed Wrong Number");
+}
